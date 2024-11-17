9800a1e5217759a3efb41696287e2ecd2bcfbeb9
Harshit Goel <149133231+Harshit-Goel-07@users.noreply.github.com>
2024-11-17 22:10:07 +0530

    Update Matrix_Chain_Multiplication.c

--- 5/Matrix_Chain_Multiplication.java
+++ 5/Matrix_Chain_Multiplication.java
@@ -1,39 +1,70 @@
-// Yash Gupta
-// 500125397 
-class Matrix_Chain_Multiplication {
+#include <stdio.h>
+#include <limits.h>
+#include <time.h>
 
-    
-    static int minMultRec(int arr[], int i, int j) {
+// Function to print the optimal parenthesis placement
+void printOptimalParenthesis(int i, int j, int n, int *bracket, char *name) {
+    if (i == j) {
+        printf("%c", (*name)++);
+        return;
+    }
+    printf("(");
 
-        // If there is only one matrix
-        if (i + 1 == j)
-            return 0;
+    // Recursively print the left and right subsets
+    printOptimalParenthesis(i, *((bracket + i * n) + j), n, bracket, name);
+    printOptimalParenthesis(*((bracket + i * n) + j) + 1, j, n, bracket, name);
 
-        int res = Integer.MAX_VALUE;
+    printf(")");
+}
 
-        for (int k = i + 1; k < j; k++) {
-            int curr = minMultRec(arr, i, k)
-                       + minMultRec(arr, k, j)
-                       + arr[i] * arr[k] * arr[j];
+// Matrix Chain Multiplication function using DP with detailed cost printing
+int MatrixChainOrder(int p[], int n) {
+    int m[n][n];       // Minimum number of multiplications table
+    int bracket[n][n]; // Stores optimal split point for printing order
 
-            res = Math.min(curr, res);
+    // Initialize main diagonal to zero
+    for (int i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L is the chain length
+    for (int L = 2; L < n; L++) {
+        for (int i = 1; i < n - L + 1; i++) {
+            int j = i + L - 1;
+            m[i][j] = INT_MAX;
+
+            printf("Calculating cost for matrices from A%d to A%d\n", i, j);
+            for (int k = i; k <= j - 1; k++) {
+                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                printf("Split at A%d: Cost = %d\n", k, q);
+                if (q < m[i][j]) {
+                    m[i][j] = q;
+                    bracket[i][j] = k; // Store split point in bracket
+                }
+            }
+            printf("Minimum cost for multiplying A%d to A%d is %d\n\n", i, j, m[i][j]);
         }
-
-        // Return minimum count
-        return res;
     }
 
-    static int matrixMultiplication(int arr[]) {
-        
-        int n = arr.length;
-        return minMultRec(arr, 0, n - 1);
-    }
+    // Display optimal order of multiplication
+    char name = 'A';
+    printf("Optimal Parenthesization is: ");
+    printOptimalParenthesis(1, n - 1, n, (int *)bracket, &name);
+    printf("\n");
 
-    public static void main(String[] args) {
-      
-        int arr[] = {40, 20, 30, 10, 30};// Matrix Array has dimension arr[i-1] x arr[i]
-        /* For this case we will have -- 40X20 20X30 30X10  10X30 */
-        int res = matrixMultiplication(arr);
-        System.out.println("The minimum count of scalar multiplication will be"+res);
-    }
+    return m[1][n - 1]; // Return minimum cost of multiplication
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4}; // Dimensions of matrices A1 (1x2), A2 (2x3), A3 (3x4)
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    clock_t start = clock();
+    int min_cost = MatrixChainOrder(arr, size);
+    clock_t end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Minimum number of multiplications is %d\n", min_cost);
+    printf("Time taken to compute optimal parenthesization: %f seconds\n", time_taken);
+
+    return 0;
 }
